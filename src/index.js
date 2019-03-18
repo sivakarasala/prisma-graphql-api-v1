8c212bc52f7d5f4d527613a4099e10f4cc193386
shambho <Sivakrishna.Karasala@eku.edu>
2019-03-18 16:22:10 -0400

    add post query

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { GraphQLServer } from "graphql-yoga";
 const typeDefs = `
   type Query {
     me:User!
+    post: Post!
   }
 
   type User {
@@ -11,6 +12,13 @@ const typeDefs = `
     name: String!
     email: String!
     age: Int
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
   }
 `;
 
@@ -23,6 +31,14 @@ const resolvers = {
         name: "shiva",
         email: "shiva@shiva.com",
         age: 29
+      };
+    },
+    post() {
+      return {
+        id: "098",
+        title: "Adi yogi",
+        body: "",
+        published: false
       };
     }
   }
